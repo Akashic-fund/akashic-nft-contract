8e1b7b09396de7da86af1bd07a2d812cd5847ba3
Shikhar Singh <0xshikhar@gmail.com>
2025-02-27 22:59:18 +0530

    feat: add campaign factory contract

--- README.md
+++ README.md
@@ -41,18 +41,6 @@ This template comes with sensible default configurations in the following files:
 This template is IDE agnostic, but for the best user experience, you may want to use it in VSCode alongside Nomic
 Foundation's [Solidity extension](https://marketplace.visualstudio.com/items?itemName=NomicFoundation.hardhat-solidity).
 
-### GitHub Actions
-
-This template comes with GitHub Actions pre-configured. Your contracts will be linted and tested on every push and pull
-request made to the `main` branch.
-
-Note though that to make this work, you must use your `INFURA_API_KEY` and your `MNEMONIC` as GitHub secrets.
-
-For more information on how to set up GitHub secrets, check out the
-[docs](https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions).
-
-You can edit the CI script in [.github/workflows/ci.yml](./.github/workflows/ci.yml).
-
 ## Usage
 
 ### Pre Requisites
---
+++ contracts/CampaignNFTFactory.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./CampaignNFT.sol";
+
+/**
+ * @title CampaignNFTFactory
+ * @dev Factory contract for deploying campaign-specific NFT contracts
+ */
+contract CampaignNFTFactory is Ownable {
+    // Store all deployed campaign NFT contracts
+    mapping(string => address) public campaignNFTs;
+
+    // Events
+    event CampaignNFTCreated(
+        string indexed campaignId,
+        address nftContract,
+        address campaignOwner,
+        address campaignTreasury
+    );
+
+    /**
+     * @dev Create a new NFT contract for a campaign
+     * @param campaignId Unique identifier for the campaign
+     * @param campaignName Name of the campaign
+     * @param symbol Symbol for the NFT token
+     * @param defaultTokenURI IPFS link to the NFT metadata
+     * @param minDonationAmount Minimum donation required for NFT eligibility
+     * @param campaignOwner Address of the campaign owner
+     * @param campaignTreasury Address of the campaign treasury contract
+     * @return Address of the newly deployed NFT contract
+     */
+    function createCampaignNFT(
+        string memory campaignId,
+        string memory campaignName,
+        string memory symbol,
+        string memory defaultTokenURI,
+        uint256 minDonationAmount,
+        address campaignOwner,
+        address campaignTreasury
+    ) external onlyOwner returns (address) {
+        require(campaignNFTs[campaignId] == address(0), "Campaign NFT already exists");
+
+        CampaignNFT newNFT = new CampaignNFT(
+            campaignId,
+            campaignName,
+            symbol,
+            defaultTokenURI,
+            minDonationAmount,
+            campaignOwner,
+            campaignTreasury
+        );
+
+        address nftAddress = address(newNFT);
+        campaignNFTs[campaignId] = nftAddress;
+
+        emit CampaignNFTCreated(campaignId, nftAddress, campaignOwner, campaignTreasury);
+
+        return nftAddress;
+    }
+
+    /**
+     * @dev Get the NFT contract address for a specific campaign
+     * @param campaignId Unique identifier for the campaign
+     * @return Address of the campaign's NFT contract
+     */
+    function getCampaignNFT(string memory campaignId) external view returns (address) {
+        return campaignNFTs[campaignId];
+    }
+}
