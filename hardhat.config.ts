1814d24ed37b1b8e34f1939fa79d8619da7afae8
Shikhar Singh <0xshikhar@gmail.com>
2025-03-05 15:39:19 +0530

    feat: add sepolia support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,7 @@ import "./tasks/accounts";
 
 // Run 'npx hardhat vars setup' to see the list of variables that need to be set
 
-const mnemonic: string = vars.get("MNEMONIC");
-const infuraApiKey: string = vars.get("INFURA_API_KEY");
+const privateKey: string = vars.get("PRIVATE_KEY");
 
 const chainIds = {
   "arbitrum-mainnet": 42161,
@@ -22,6 +21,8 @@ const chainIds = {
   "polygon-mainnet": 137,
   "polygon-mumbai": 80001,
   sepolia: 11155111,
+  celo: 42220,
+  celoTestnet: 44787,
 };
 
 function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
@@ -33,15 +34,21 @@ function getChainConfig(chain: keyof typeof chainIds): NetworkUserConfig {
     case "bsc":
       jsonRpcUrl = "https://bsc-dataseed1.binance.org";
       break;
+    case "celo":
+      jsonRpcUrl = "https://rpc.celo.org";
+      break;
+
+    case "sepolia":
+      jsonRpcUrl = "https://eth-sepolia.g.alchemy.com/v2/WddzdzI2o9S3COdT73d5w6AIogbKq4X-";
+      break;
+    case "celoTestnet":
+      jsonRpcUrl = "https://alfajores-forno.celo-testnet.org";
+      break;
     default:
-      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + infuraApiKey;
+      jsonRpcUrl = "https://" + chain + ".infura.io/v3/" + vars.get("INFURA_API_KEY");
   }
   return {
-    accounts: {
-      count: 10,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
+    accounts: [privateKey],
     chainId: chainIds[chain],
     url: jsonRpcUrl,
   };
@@ -73,17 +80,20 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
-        mnemonic,
+        mnemonic: vars.get("MNEMONIC"),
       },
       chainId: chainIds.hardhat,
     },
     ganache: {
       accounts: {
-        mnemonic,
+        mnemonic: vars.get("MNEMONIC"),
       },
       chainId: chainIds.ganache,
       url: "http://localhost:8545",
     },
+    celo: getChainConfig("celo"),
+    sepolia: getChainConfig("sepolia"),
+    celoTestnet: getChainConfig("celoTestnet"),
     arbitrum: getChainConfig("arbitrum-mainnet"),
     avalanche: getChainConfig("avalanche"),
     bsc: getChainConfig("bsc"),
@@ -91,7 +101,6 @@ const config: HardhatUserConfig = {
     optimism: getChainConfig("optimism-mainnet"),
     "polygon-mainnet": getChainConfig("polygon-mainnet"),
     "polygon-mumbai": getChainConfig("polygon-mumbai"),
-    sepolia: getChainConfig("sepolia"),
   },
   paths: {
     artifacts: "./artifacts",
